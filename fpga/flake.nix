a3fdff9147268a99e8bf840096cf7c9449c752d7
dingduck <kfove.liang@gmail.com>
2024-07-06 12:36:41 +0800

    24.05 版本下的 synlig 不可直接使用，然后unstable因为版本泰兴编译不通过

--- fpga/flake.nix
+++ fpga/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
@@ -11,7 +11,24 @@
     flake-utils.lib.eachDefaultSystem
     (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowBroken = true;
+          overlays = [overlay];
+        };
+        overlay = final: prev: {
+          yosys-synlig = prev.yosys-synlig.overrideAttrs (
+            final: prev: {
+              installPhase = ''
+                runHook preInstall
+                mkdir -p $out/share/yosys/plugins
+                cp ./build/release/systemverilog-plugin/systemverilog.so \
+                  $out/share/yosys/plugins/synlig.so
+                runHook postInstall
+              '';
+            }
+          );
+        };
       in {
         # flake contents here
         devShells = {
@@ -19,7 +36,8 @@
             buildInputs = with pkgs; [
               openfpgaloader
               python312Packages.apycula
-              yosys
+              (yosys.withPlugins [yosys-synlig])
+              # yosys-synlig
               nextpnrWithGui
             ];
           };
