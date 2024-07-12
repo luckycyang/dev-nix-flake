db8f741c60bcc7131c08cc46fa061043a4e1baf3
luckycyang <166975192+luckycyang@users.noreply.github.com>
2024-07-12 14:34:13 +0800

    Update flake.nix
    stack 的脚本目前有，根本没有 ghc966, 可能需要 overlay

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -1,36 +1,27 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
     nixpkgs,
-    devenv,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
-
-    devShells.${system}.default = devenv.lib.mkShell {
-      inherit inputs pkgs;
-      modules = [
-        ({
-          pkgs,
-          config,
-          ...
-        }: {
-          # This is your devenv configuration
-          enterShell = ''
-            echo Welcome developing haskell
-          '';
-          languages.haskell.enable = true;
-        })
-      ];
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        # flake contents here
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cabal-install
+              ghc
+            ];
+          };
+        };
+      }
+    );
 }
